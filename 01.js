801c8b850daded56c065abaca77b694e870aa792
ws <286572050@qq.com>
2022-08-04 23:10:25 +0800

    'day3'

---
+++ 01.js
@@ -0,0 +1,24 @@
+// 2、模式 
+//     normal模式 insert模式
+// 2、基本移动 上下左右 HJKL
+// 3、插入文字 i光标后方 a光标前方
+// 4、推出insert模式
+// 5、在终端中如何退出
+//     :wq 保存并退出  :q! 强制退出
+// 6、ctrl和caps调换位置 
+//     系统键盘修改修饰键
+// 7、快速移动 
+//     配置 以及 修改键盘设置
+    
+// tips： 习惯insert完立即回到Norma模式
+
+sdadasdasdasdas
+asdasdasdasdasdsd
+dasdasda 
+
+
+const a = ''
+
+
+const bbbbbbbbbbbbbbbbb = 2222222222222222;
+
